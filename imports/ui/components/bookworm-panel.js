b7acaf51236ec742a87244a2c64b98a22c0df7b1
Felix Su <felixs8696@berkeley.edu>
2016-07-26 17:07:53 -0400

    each panel has own background

---
+++ imports/ui/components/bookworm-panel.js
@@ -0,0 +1,21 @@
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import "./bookworm-panel.html";
+
+Template.Bookworm_panel.onCreated(function bookwormPanelOnCreated() {
+
+});
+
+Template.Bookworm_panel.onRendered(function() {
+
+});
+
+Template.Bookworm_panel.helpers({
+
+
+});
+
+Template.Bookworm_panel.events({
+
+});
