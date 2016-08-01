8897a3d2e121770d3725c1133dc4e7dde62ef6a0
Felix Su <felixs8696@berkeley.edu>
2016-08-01 16:05:15 -0400

    basic text formatting

--- imports/ui/components/dynamo-panel.js
+++ imports/ui/components/dynamo-panel.js
@@ -4,12 +4,12 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import "./dynamo-panel.html";
 
 var organizations = [
-  {title: 'Berkeley ABA' ,img: '/organizations/aba.png', style: 'square', url: 'http://www.berkeleyaba.com', animInt: 3},
-  {title: 'CalHacks 2.0' ,img: '/organizations/calhacks.png', style: 'square', url: 'http://www.calhacks.io', animInt: 4},
-  {title: 'UC Berkeley EECS Department' ,img: '/organizations/eecs.png', style: 'rect', url: 'https://eecs.berkeley.edu', animInt: 5},
-  {title: 'Cal Alumni Student Association' ,img: '/organizations/casa.png', style: 'square', url: 'http://alumni.berkeley.edu/community/student-programs/cal-alumni-student-association', animInt: 6},
-  {title: 'European Innocation Academy' ,img: '/organizations/eia.png', style: 'rect', url: 'http://inacademy.eu/', animInt: 7},
-  {title: 'Intramural Basketball' ,img: '/organizations/intramural.jpg', style: 'square', url: 'http://recsports.berkeley.edu/sports/intramural-sports/sport-specific-rules/basketball-5-on-5/', animInt: 8}
+  {title: 'Berkeley ABA | Media Technology Chair' ,img: '/organizations/aba.png', style: 'square', url: 'http://www.berkeleyaba.com', animInt: 3},
+  {title: 'CalHacks 2.0 | Participant' ,img: '/organizations/calhacks.png', style: 'square', url: 'http://www.calhacks.io', animInt: 4},
+  {title: 'UC Berkeley EECS Department | CS61A/CS61B Lab Assistant' ,img: '/organizations/eecs.png', style: 'rect', url: 'https://eecs.berkeley.edu', animInt: 5},
+  {title: 'Cal Alumni Student Association | Web Content Coordinator' ,img: '/organizations/casa.png', style: 'square', url: 'http://alumni.berkeley.edu/community/student-programs/cal-alumni-student-association', animInt: 6},
+  {title: 'European Innovation Academy | Hack In CEO' ,img: '/organizations/eia.png', style: 'rect', url: 'http://inacademy.eu/', animInt: 7},
+  {title: 'Intramural Basketball | Point Guard' ,img: '/organizations/intramural.jpg', style: 'square', url: 'http://recsports.berkeley.edu/sports/intramural-sports/sport-specific-rules/basketball-5-on-5/', animInt: 8}
 ]
 
 Template.Dynamo_panel.onCreated(function dynamoPanelOnCreated() {
