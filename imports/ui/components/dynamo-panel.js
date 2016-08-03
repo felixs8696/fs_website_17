084e561676d8c7085f0a49efa41d5e4cb59a017d
Felix Su <felixs8696@berkeley.edu>
2016-08-02 22:34:53 -0400

    added linkedin pulse

--- imports/ui/components/dynamo-panel.js
+++ imports/ui/components/dynamo-panel.js
@@ -7,9 +7,10 @@ var organizations = [
   {title: 'Berkeley ABA | Media Technology Chair' ,img: '/organizations/aba.png', style: 'square', url: 'http://www.berkeleyaba.com', animInt: 3},
   {title: 'CalHacks 2.0 | Participant' ,img: '/organizations/calhacks.png', style: 'square', url: 'http://www.calhacks.io', animInt: 4},
   {title: 'UC Berkeley EECS Department | CS61A/CS61B Lab Assistant' ,img: '/organizations/eecs.png', style: 'rect', url: 'https://eecs.berkeley.edu', animInt: 5},
-  {title: 'Cal Alumni Student Association | Web Content Coordinator' ,img: '/organizations/casa.png', style: 'square', url: 'http://alumni.berkeley.edu/community/student-programs/cal-alumni-student-association', animInt: 6},
-  {title: 'European Innovation Academy | Hack In CEO' ,img: '/organizations/eia.png', style: 'rect', url: 'http://inacademy.eu/', animInt: 7},
-  {title: 'Intramural Basketball | Point Guard' ,img: '/organizations/intramural.jpg', style: 'square', url: 'http://recsports.berkeley.edu/sports/intramural-sports/sport-specific-rules/basketball-5-on-5/', animInt: 8}
+  {title: 'LinkedIn Pulse | Writer' ,img: '/organizations/pulse.png', style: 'square', url: 'https://www.linkedin.com/pulse/todays-small-players-tomorrows-big-winners-felix-su?trk=prof-post', animInt: 6},
+  {title: 'Cal Alumni Student Association | Web Content Coordinator' ,img: '/organizations/casa.png', style: 'square', url: 'http://alumni.berkeley.edu/community/student-programs/cal-alumni-student-association', animInt: 7},
+  {title: 'European Innovation Academy | Hack In CEO' ,img: '/organizations/eia.png', style: 'rect', url: 'http://inacademy.eu/', animInt: 8},
+  {title: 'Intramural Basketball | Point Guard' ,img: '/organizations/intramural.png', style: 'square', url: 'http://recsports.berkeley.edu/sports/intramural-sports/sport-specific-rules/basketball-5-on-5/', animInt: 9}
 ]
 
 Template.Dynamo_panel.onCreated(function dynamoPanelOnCreated() {
